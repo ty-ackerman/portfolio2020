071f11cf5d16b50d3bcc9055ac9202b45fbe39cf
Ty Ackerman <ty.ackerman@Tys-MacBook-Pro.hitronhub.home>
2020-03-28 01:15:02 -0400

    Styled some post contents + added next/previous buttons

--- src/Components/PostContent.js
+++ src/Components/PostContent.js
@@ -1,76 +1,90 @@
-import React from 'react'
-import styled from "styled-components"
-
+import React from 'react';
+import styled from 'styled-components';
 
 export function PostContent(props) {
-    console.log(props)
-    return (
-        <div className={`post-${props.type}`}>
-            {props.content}
-        </div>
-    )
+	const Paragraph = styled.div`margin: 14px 0;`;
+	return <Paragraph className={`post-${props.type}`}>{props.content}</Paragraph>;
 }
 
 export function PostSubtitle(props) {
-    const Subtitle = styled.h2`
-        // color: red;
-    `
+	const Subtitle = styled.h2`margin: 20px 0;`;
 
-    return (
-        <div>
-            <Subtitle>{props.content}</Subtitle>
-        </div>
-    )
-} 
+	return (
+		<div>
+			<Subtitle>{props.content}</Subtitle>
+		</div>
+	);
+}
+
+export function PostBlockquote(props) {
+	const Blockquote = styled.div`
+		margin: 20px 0;
+		padding-left: 30px;
+		position: relative;
+		font-style: italic;
+		&:before {
+			width: 1px;
+			height: calc(100% - 10px);
+			content: "";
+			background-color: #dfdfdf;
+			position: absolute;
+			left: 0;
+			top: 50%;
+			transform: translateY(-50%);
+		}
+	`;
+
+	return (
+		<div>
+			<Blockquote>{props.content}</Blockquote>
+		</div>
+	);
+}
 
 export function PostListOrdered(props) {
-    return (
-        <div>
-            <ol>
-                {props.content.map((item, key) => {
-                    return <li key={key}>{item}</li>
-                })}
-            </ol>
-        </div>
-    )
+	return (
+		<div>
+			<ol>
+				{props.content.map((item, key) => {
+					return <li key={key}>{item}</li>;
+				})}
+			</ol>
+		</div>
+	);
 }
 
 export function PostListUnordered(props) {
-    return (
-        <div>
-            <ul>
-                {props.content.map((item, key) => {
-                    return <li key={key}>{item}</li>
-                })}
-            </ul>
-        </div>
-    )
+	return (
+		<div>
+			<ul>
+				{props.content.map((item, key) => {
+					return <li key={key}>{item}</li>;
+				})}
+			</ul>
+		</div>
+	);
 }
-
-
-
 
 export function PostPhoto(props) {
-    const imageWidth = () => {
-        switch (props.size) {
-            case "sm":
-                return '25%'
-            case "md":
-                return '50%'
-            case "lg":
-                return '100%'
-        }
-    }
-    
-    const PhotoContainer = styled.div`
-        width: ${imageWidth()};
-    `
+	const imageWidth = () => {
+		switch (props.size) {
+			case 'sm':
+				return '25%';
+			case 'md':
+				return '50%';
+			case 'lg':
+				return '100%';
+		}
+	};
 
-    return (
-        <PhotoContainer className={`post-picture post-picture-${props.size}`}>
-            <img style={{maxWidth: "100%"}} src={props.content} alt={props.altText}/>
-        </PhotoContainer>
-    )
+	const PhotoContainer = styled.div`
+		width: ${imageWidth()};
+		margin: 30px 0;
+	`;
+
+	return (
+		<PhotoContainer className={`post-picture post-picture-${props.size}`}>
+			<img src={props.content} alt={props.altText} />
+		</PhotoContainer>
+	);
 }
-
-
--- src/Sections/WritingPost.js
+++ src/Sections/WritingPost.js
@@ -2,28 +2,42 @@ import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import queryString from 'query-string';
 import { isEmpty } from 'lodash';
-import { PostContent, PostPhoto, PostSubtitle, PostListOrdered, PostListUnordered } from '../Components/PostContent';
+import {
+	PostContent,
+	PostPhoto,
+	PostSubtitle,
+	PostListOrdered,
+	PostListUnordered,
+	PostBlockquote
+} from '../Components/PostContent';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import Posts from '../Writing/Posts.json';
 
 function WritingPost(props) {
+	const [ posts, setPosts ] = useState({});
 	const [ post, setPost ] = useState({});
 
 	useEffect(() => {
 		getPost();
-	}, []);
+		window.scrollTo(0, 0);
+	});
 
 	const getPost = () => {
 		const postId = queryString.parse(props.location.search);
-		const currPost = Posts.filter((post) => post.path === postId.id);
-
+		const indexedPosts = Posts.map((post, key) => {
+			post.index = key;
+			return post;
+		});
+		const currPost = indexedPosts.filter((post) => post.path === postId.id);
 		setPost(...currPost);
+		setPosts(Posts);
 	};
 
 	const renderContent = (section, key) => {
 		switch (section.type) {
 			case 'paragraph':
-			case 'blockquote':
 				return <PostContent key={key} {...section} />;
 			case 'photo':
 				return <PostPhoto key={key} {...section} />;
@@ -33,17 +47,94 @@ function WritingPost(props) {
 				return <PostListOrdered key={key} {...section} />;
 			case 'ul':
 				return <PostListUnordered key={key} {...section} />;
+			case 'blockquote':
+				return <PostBlockquote key={key} {...section} />;
 		}
 	};
+
+	const WritingPost = styled.div`
+		display: flex;
+		flex-flow: column wrap;
+		.post-contents {
+			margin: 0 auto;
+			max-width: 600px;
+			width: 100%;
+		}
+		h1 {
+			font-size: 35px;
+			font-weight: 500;
+			margin-bottom: 10px;
+		}
+		.date {
+			color: #797979;
+			padding-top: 10px;
+			position: relative;
+			&:before {
+				height: 1px;
+				width: 40px;
+				content: '';
+				background-color: #dfdfdf;
+				position: absolute;
+				top: 0;
+			}
+		}
+		.post-header {
+			margin-bottom: 45px;
+		}
+		.change-post {
+			display: flex;
+			justify-content: ${post.index === 0 ? 'flex-end' : 'space-between'};
+		}
+		.next,
+		.previous {
+			width: 20%;
+			cursor: pointer;
+			padding-top: 20px;
+			position: relative;
+			margin-top: 80px;
+			border-top: 1px solid #dfdfdf;
+			.change-title {
+				color: #797979;
+			}
+			transition: all .15s linear;
+			&:hover .change-post-name {
+				text-decoration: underline;
+			}
+		}
+		.next {
+			text-align: right;
+		}
+	`;
+
+	const loadNewPost = (postPath) => {
+		// getPost();
+		props.history.push(`/writing/post?id=${postPath}`);
+	};
+
 	return (
-		<div className="section">
-			<div>
-				<div className="title">{post.title}</div>
-				<div className="subtitle">{post.description}</div>
+		<WritingPost className="section">
+			<div className="post-contents post-header">
+				<h1 className="title">{post.title}</h1>
 				<div className="date">{post.dateLong}</div>
 			</div>
-			<div>{!isEmpty(post) && post.sections.map((section, key) => renderContent(section, key))}</div>
-		</div>
+			<div className="post-contents">
+				{!isEmpty(post) && post.sections.map((section, key) => renderContent(section, key))}
+			</div>
+			<div className="change-post">
+				{post.index > 0 && (
+					<div className="previous" onClick={() => loadNewPost(posts[post.index - 1].path)}>
+						<div className="change-title">Previous</div>
+						<div className="change-post-name">{posts[post.index - 1].title}</div>
+					</div>
+				)}
+				{post.index < posts.length - 1 && (
+					<div className="next" onClick={() => loadNewPost(posts[post.index + 1].path)}>
+						<div className="change-title">Next</div>
+						<div className="change-post-name">{posts[post.index + 1].title}</div>
+					</div>
+				)}
+			</div>
+		</WritingPost>
 	);
 }
 
