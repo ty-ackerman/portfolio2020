75f128ff2bd62a71af1aa9b9d7e1cd1a1f926ebf
Ty Ackerman <tyler.raye.ackerman@gmail.com>
2020-04-09 03:25:50 -0400

    Changed style of tattoo page + added more responsiveness

--- src/Components/TattooContent.js
+++ src/Components/TattooContent.js
@@ -1,99 +1,111 @@
-import React from 'react';
-import { css } from '@emotion/core';
-import ClockLoader from 'react-spinners/ClockLoader';
-import styled from 'styled-components';
+import React from 'react'
+import { css } from '@emotion/core'
+import ClockLoader from 'react-spinners/ClockLoader'
+import styled from 'styled-components'
 import Fade from './Fade'
 
-export default function TattooContent(props) {
-	const { content, type, redirect } = props;
+export default function TattooContent (props) {
+  const { content, type, redirect } = props
 
-	const isMobile = () => {
-		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-			return true;
-		}
-		return false;
-	};
+  const isMobile = () => {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      return true
+    }
+    return false
+  }
 
-	const handleContent = (content, type) => {
-		switch (type) {
-			case 'picture':
-				return (
-					<div style={{ maxWidth: isMobile() ? '100%' : '50%' }}>
-						<img src={content} alt="" />
-					</div>
-				);
-			case 'website':
-				if (redirect) {
-					window.location = content;
-				}
-				return (
-					<div style={{ maxWidth: '80%' }}>
-						<div>QR code redirects to:</div>
-						<div style={{ paddingTop: '10px' }}>
-							<a target="_blank" rel="noopener noreferrer" href={content}>
-								{content}
-							</a>
-						</div>
-					</div>
-				);
-			case 'video':
-				const id = getYoutubeId(content);
-				return (
-					<iframe
-						width="560"
-						height="315"
-						src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=1`}
-						frameBorder="0"
-						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-						webkitallowfullscreen="true"
-						mozallowfullscreen="true"
-						allowFullScreen={true}
-					/>
-				);
-				break;
+  const handleContent = (content, type) => {
+    switch (type) {
+      case 'picture':
+        return (
+          <div style={{ maxWidth: '100%' }}>
+            <img src={content} alt='' />
+          </div>
+        )
+      case 'website':
+        if (redirect) {
+          window.location = content
+        }
+        return (
+          <div style={{ maxWidth: '80%' }}>
+            <div>QR code redirects to:</div>
+            <div style={{ paddingTop: '10px' }}>
+              <a target='_blank' rel='noopener noreferrer' href={content}>
+                {content}
+              </a>
+            </div>
+          </div>
+        )
+      case 'video':
+        const id = getYoutubeId(content)
+        return (
+          <iframe
+            width='560'
+            height='315'
+            src={`https://www.youtube.com/embed/${id}?rel=0&autoplay=1`}
+            frameBorder='0'
+            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+            webkitallowfullscreen='true'
+            mozallowfullscreen='true'
+            allowFullScreen={true}
+          />
+        )
+        break
 
-			default:
-				break;
-		}
-	};
+      default:
+        break
+    }
+  }
 
-	const getYoutubeId = (url) => {
-		const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-		const match = url.match(regExp);
+  const getYoutubeId = url => {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+    const match = url.match(regExp)
 
-		return match && match[2].length === 11 ? match[2] : null;
-	};
+    return match && match[2].length === 11 ? match[2] : null
+  }
 
-	const override = css`margin: 0 auto;`;
+  const override = css`
+    margin: 0 auto;
+  `
 
-	const LoadingContainer = styled.div`
-		height: 300px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	`;
+  const LoadingContainer = styled.div`
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
 
+  const TattooContentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 40px 0;
+  `
 
-	const TattooContentContainer = styled.div`
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin: 40px 0;
-	`;
+  if (content && type) {
+    return (
+      <Fade show>
+        <TattooContentContainer>
+          {handleContent(content, type)}
+        </TattooContentContainer>
+      </Fade>
+    )
+  }
 
-	if (content && type) {
-		return (
-			<Fade show>
-				<TattooContentContainer>{handleContent(content, type)}</TattooContentContainer>
-			</Fade >
-		);
-	}
-
-	return (
-		<Fade show={false} >
-			<LoadingContainer>
-				<ClockLoader css={override} size={50} color={'#000000'} loading={true} />
-			</LoadingContainer>
-		</Fade >
-	);
+  return (
+    <Fade show={false}>
+      <LoadingContainer>
+        <ClockLoader
+          css={override}
+          size={50}
+          color={'#000000'}
+          loading={true}
+        />
+      </LoadingContainer>
+    </Fade>
+  )
 }
--- src/Sections/Tattoo.js
+++ src/Sections/Tattoo.js
@@ -7,11 +7,100 @@ import Fade from '../Components/Fade'
 import TattooContent from '../Components/TattooContent'
 import { useSwipeable, Swipeable } from 'react-swipeable'
 import { withRouter } from 'react-router-dom'
+import styled from 'styled-components'
 
-const description =
+const content =
   'The hardest part about getting a tattoo is choosing a permanent design. My solution - a QR code that scans to the content of my choosing by linking it to my personal website. To put it more simply, I can change what the code displays with only a few clicks.'
 
-const subtitle = 'So What Does It Scan To?'
+const subtitle = 'Current Content'
+
+const title = 'QR Code Tattoo'
+const description = 'Permanent Design - Temporary Content'
+
+const PostContentsStyled = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: 850px) {
+    flex-direction: column;
+    }
+  }
+  .left,
+  .right {
+    width: 50%;
+    @media (max-width: 850px) {
+      width: 100%;
+    }
+  }
+  h2 {
+    margin-top: 20px;
+  }
+
+  .left {
+    padding-right: 20px;
+    display: flex;
+    flex-flow: column wrap;
+    font-size: 18px;
+    @media (max-width: 1100px) {
+      font-size: 16px;
+      h2 {
+        font-size: 30px;
+      }
+      h3 {
+        font-size: 20px;
+      }
+    }
+    @media (max-width: 850px) {
+      padding-right: 0;
+    }
+    h2 {
+      margin-top: 20px;
+      font-size: 36px;
+    }
+    h3 {
+      font-weight: 400;
+      font-size: 22px;
+      position: relative;
+      padding-bottom: 15px;
+      margin-bottom: 5px;
+      &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        width: 50px;
+        height: 1px;
+        background-color: #dfdfdf;
+        left: 0;
+      }
+    }
+    .content-info {
+      padding: 15px 0;
+      @media(max-width: 850px) {
+        text-align: justify;
+      }
+    }
+    .learn-more {
+      margin-top: 50px;
+      margin-bottom: 20px;
+      @media (max-width: 850px) {
+        margin-top: 20px;
+        margin-bottom: 10px;
+      }
+      a {
+        text-decoration: underline;
+      }
+    }
+  }
+  .right {
+    padding-left: 20px;
+        @media (max-width: 850px) {
+      padding-left: 0;
+    }
+  }
+`
+
+const SectionStyled = styled.div`
+  display: flex;
+`
 
 export class Tattoo extends Component {
   constructor () {
@@ -77,25 +166,28 @@ export class Tattoo extends Component {
     return (
       <Swipeable onSwipedLeft={eventData => this.handleSwipe()}>
         <Fade show>
-          <div className='section' onClick={this.handleClick}>
-            <div className='post-contents'>
-              <Title
-                title='My QR Code Tattoo'
-                subtitle='Permanent tattoo, temporary content.'
-              />
-              <PostContent content={description} />
-              <PostSubtitle content={subtitle} />
-              <TattooContent
-                content={this.state.content}
-                type={this.state.type}
-                redirect={this.props.redirect}
-              />
-              <div>
-                To learn more about the tattoo,{' '}
-                <Link to='/writing/post?id=tattoo'>click here</Link>.
+          <SectionStyled className='section' onClick={this.handleClick}>
+            <PostContentsStyled>
+              <div className='left'>
+                <h2>{title}</h2>
+                <h3>{description}</h3>
+                <div className='content-info'>{content}</div>
+                <div className='learn-more'>
+                  <div className='div'>
+                    To learn more about the tattoo,{' '}
+                    <Link to='/writing/post?id=tattoo'>click here</Link>.
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+              <div className='right'>
+                <TattooContent
+                  content={this.state.content}
+                  type={this.state.type}
+                  redirect={this.props.redirect}
+                />
+              </div>
+            </PostContentsStyled>
+          </SectionStyled>
         </Fade>
       </Swipeable>
     )
