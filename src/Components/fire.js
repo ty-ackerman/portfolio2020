f4b98df861946c8e1bff50e2b3495ca6e0cb0899
Ty Ackerman <tyler.raye.ackerman@gmail.com>
2020-04-14 01:33:22 -0400

    Removed API key + post typo

--- src/Components/fire.js
+++ src/Components/fire.js
@@ -1,22 +1,26 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyAwrkUe08V-oBzH6kuudAmwEFdi4FIVqDA",
-    authDomain: "qr-tattoo.firebaseapp.com",
-    databaseURL: "https://qr-tattoo.firebaseio.com",
-    projectId: "qr-tattoo",
-    storageBucket: "qr-tattoo.appspot.com",
-    messagingSenderId: "132370008483",
-    appId: "1:132370008483:web:810c72d5ec137f496bb216",
-    measurementId: "G-N7DP12M92Y"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'qr-tattoo.firebaseapp.com',
+  databaseURL: 'https://qr-tattoo.firebaseio.com',
+  projectId: 'qr-tattoo',
+  storageBucket: 'qr-tattoo.appspot.com',
+  messagingSenderId: '132370008483',
+  appId: '1:132370008483:web:810c72d5ec137f496bb216',
+  measurementId: 'G-N7DP12M92Y'
+}
 
-var fire = firebase.initializeApp(config);
-  
-export default fire;
+var fire = firebase.initializeApp(config)
+
+export default fire
 
 export const postData = async (content, type) => {
-    fire.database().ref('/').set({
-      content, type
-    });
+  fire
+    .database()
+    .ref('/')
+    .set({
+      content,
+      type
+    })
 }
